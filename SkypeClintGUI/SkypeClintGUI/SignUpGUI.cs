42a774966b660d2d98db0dae0a0324b9ac61cdff
Khandker M Qaiduzzaman <nafees.ku11@gmail.com>
2020-01-19 00:25:47 +0100

    Create SignUpGUI.cs

---
+++ SkypeClintGUI/SkypeClintGUI/SignUpGUI.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SkypeClintGUI
+{
+    public partial class SignUpGUI : Form
+    {
+        public SignUpGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_name_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox_birthDay_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox_birthMonth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_Email_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_sighUp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void label_appName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label_SIGNUP_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label_name_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label_password_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_appID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox_birthYear_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox_gender_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+      
+    }
+}
